a80a0c35f7a29771be3acd2fd976a7f0e41d5cc6
bezzz <bezzz2304@gmail.com>
2024-04-19 14:44:35 +0300

    CCL-4236 auto iframe height, dev prices

--- src/components/Prices/index.js
+++ src/components/Prices/index.js
@@ -9,12 +9,18 @@ export default function Prices() {
   const titleRu = 'Цена';
   const titleEn = 'Price';
   const title = isRULocale ? titleRu : titleEn;
-  const priceLink = isRULocale ? 'https://capmonster.cloud/prices?culture=ru-RU' : 'https://capmonster.cloud/prices?culture=en-US';
+  const priceLink = isRULocale ? 'https://cmadmin.dev.k8s.zenno.services/prices?culture=ru-RU' : 'https://cmadmin.dev.k8s.zenno.services/prices?culture=en-US';
 
   const subTitle = isRULocale ? 'Прокси включены в цену*: экономьте с CapMonster Cloud' : 'Proxy price inclusions*: save with CapMonster Cloud';
 
   const priceSubTooltipText = isRULocale ? 'За исключением заданий Cloudflare Bot Challenge, Cloudflare Turnstile' : 'Excluding Cloudflare Bot Challenge, Cloudflare Turnstile';
   
+
+  const ref = React.useRef();
+  const [height, setHeight] = React.useState("0px");
+  const onLoad = () => {
+    setHeight(ref.current?.contentWindow ? ref.current?.contentWindow?.document.body.scrollHeight + "px" : 0);
+  };
 
   return (
     <section className={styles.pricesWrap}>
@@ -25,7 +31,7 @@ export default function Prices() {
           {priceSubTooltipText}
         </Tooltip>
         <div className={styles.prices}>
-          <iframe src={priceLink} />
+          <iframe src={priceLink} onLoad={onLoad} height={height} />
         </div>
       </div>
     </section>
