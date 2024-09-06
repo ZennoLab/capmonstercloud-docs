b9dd15de822211236c88e298a3ca1c7d797c805c
bezzz <bezzz2304@gmail.com>
2024-09-06 15:47:48 +0300

    response params update

--- src/theme/ParamItem/index.js
+++ src/theme/ParamItem/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const ParamItem = ({ title, required, type }) => {
+const ParamItem = ({ title, required, type, children, response }) => {
   return (
+    <>
     <div style={{ fontSize: '16px', fontWeight: 'bold' }}>
       <code style={{ paddingLeft: '10px', paddingRight: '10px', paddingTop: '5px', paddingBottom: '5px', marginRight: '5px' }}>{title}</code>
       <span style={{ color: 'grey', fontSize: '14px', fontWeight: 500, marginRight: '5px' }}>{'<' + type + '>'}</span>
-      {typeof required === 'string' && <span style={{ fontSize: '14px', color: 'red', fontWeight: 500 }}>{required}</span>}
-      {typeof required === 'boolean' && required && <span style={{ fontSize: '14px', color: 'red', fontWeight: 500 }}>required</span>}
-      {typeof required === 'boolean' && !required && <span style={{ fontSize: '14px', color: 'grey', fontWeight: 500 }}>optional</span>}
+      {typeof required === 'string' && !response && <span style={{ fontSize: '14px', color: 'red', fontWeight: 500 }}>{required}</span>}
+      {typeof required === 'boolean' && !response &&  required && <span style={{ fontSize: '14px', color: 'red', fontWeight: 500 }}>required</span>}
+      {!required && !response && <span style={{ fontSize: '14px', color: 'grey', fontWeight: 500 }}>optional</span>}
     </div>
+    {children && (<div style={{ paddingLeft: '24px', paddingTop: '10px' }}>
+      {children}
+    </div>)}
+    </>
   );
 };
 export default ParamItem;
