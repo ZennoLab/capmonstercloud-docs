cb8a314cc51c9674704a4760ba8016077b7b7b48
bezzz <bezzz2304@gmail.com>
2024-09-23 14:44:00 +0300

    added more captchas redesign

--- src/theme/ParamItem/index.js
+++ src/theme/ParamItem/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ParamItem = ({ title, required, type, children, response }) => {
   return (
     <>
-    <div style={{ fontSize: '16px', fontWeight: 'bold' }}>
+    <div style={{ fontSize: '16px', fontWeight: 'bold', marginBottom: '8px' }} className="paramItem">
       <code style={{ paddingLeft: '10px', paddingRight: '10px', paddingTop: '5px', paddingBottom: '5px', marginRight: '5px' }}>{title}</code>
       <span style={{ color: 'grey', fontSize: '14px', fontWeight: 500, marginRight: '5px' }}>{'<' + type + '>'}</span>
       {typeof required === 'string' && !response && <span style={{ fontSize: '14px', color: 'red', fontWeight: 500 }}>{required}</span>}
