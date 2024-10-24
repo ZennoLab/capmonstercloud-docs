ddfcb6cb250530115e5b756e28ac3285642c7b87
bezzz <bezzz2304@gmail.com>
2024-10-24 14:53:05 +0300

    CCL-5122 bugs fixes

--- src/components/Prices/index.js
+++ src/components/Prices/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.css';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { Tooltip } from 'react-tooltip'
+import { Tooltip } from 'react-tooltip';
 import getLocaleStrings from '../../locales/index';
 import { localesMappings } from '../../locales/index';
 
@@ -10,7 +10,30 @@ export default function Prices() {
   const { currentLocale } = i18n;
   const { priceTitle, priceSubTitle, priceSubTooltipText } = getLocaleStrings(currentLocale);
 
-  const priceLink = `https://capmonster.cloud/prices?culture=${localesMappings[currentLocale] || localesMappings.en}`
+  const priceLink = `https://capmonster.cloud/prices?culture=${localesMappings[currentLocale] || localesMappings.en}`;
+
+  // Используем ref для iframe
+  const iframeRef = useRef(null);
+  const [iframeHeight, setIframeHeight] = useState('auto'); // начальная высота
+
+  useEffect(() => {
+    // Функция для обработки сообщения от iframe
+    const handleIframeMessage = (event) => {
+      // Проверяем источник сообщения
+      if (event.origin === 'https://capmonster.cloud') {
+        // Обновляем высоту iframe
+        setIframeHeight(`${event.data}px`);
+      }
+    };
+
+    // Добавляем слушателя событий
+    window.addEventListener('message', handleIframeMessage);
+
+    // Удаляем слушателя при размонтировании компонента
+    return () => {
+      window.removeEventListener('message', handleIframeMessage);
+    };
+  }, []);
 
   return (
     <section className={styles.pricesWrap} id="price">
@@ -21,7 +44,12 @@ export default function Prices() {
           {priceSubTooltipText}
         </Tooltip>
         <div className={styles.prices}>
-          <iframe src={priceLink} />
+          {/* Устанавливаем высоту для iframe динамически */}
+          <iframe
+            ref={iframeRef}
+            src={priceLink}
+            style={{ width: '100%', height: iframeHeight, border: 'none' }}
+          />
         </div>
       </div>
     </section>
