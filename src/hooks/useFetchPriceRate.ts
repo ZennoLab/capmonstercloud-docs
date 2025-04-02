214cad88f41ec877348c7f29469d8830cb15069a
kirill.bushmin <kirill.bushmin@zennolab.com>
2025-04-02 16:05:42 +0300

    CLL-5934 fixed comments

--- src/hooks/useFetchPriceRate.ts
+++ src/hooks/useFetchPriceRate.ts
@@ -9,29 +9,29 @@ export const useFetchPriceRate = () => {
   const { i18n } = useDocusaurusContext();
   const { currentLocale } = i18n;
 
-  useEffect(() => {
-    const fetchPriceRate = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch('https://capmonster.cloud/currency/rate?from=usd&to=rub', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+  const fetchPriceRate = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('https://capmonster.cloud/currency/rate?from=usd&to=rub', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch prices');
-        }
-        const data: number = await response.json();
-        setPriceRate(data);
-      } catch (err) {
-        setError(err instanceof Error ? err.message : 'Unknown error');
-      } finally {
-        setLoading(false);
+      if (!response.ok) {
+        throw new Error('Failed to fetch prices');
       }
-    };
+      const data: number = await response.json();
+      setPriceRate(data);
+    } catch (err) {
+      setError(err instanceof Error ? err.message : 'Unknown error');
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchPriceRate();
   }, [currentLocale]);
 
--- src/hooks/useFetchPrices.ts
+++ src/hooks/useFetchPrices.ts
@@ -11,23 +11,23 @@ export const useFetchPrices = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchPrices = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch('https://capmonster.cloud/api/prices?all=true');
-        if (!response.ok) {
-          throw new Error('Failed to fetch prices');
-        }
-        const data: FetchPricesResponse = await response.json();
-        setPrices(data.Prices);
-      } catch (err) {
-        setError(err instanceof Error ? err.message : 'Unknown error');
-      } finally {
-        setLoading(false);
+  const fetchPrices = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('https://capmonster.cloud/api/prices?all=true');
+      if (!response.ok) {
+        throw new Error('Failed to fetch prices');
       }
-    };
+      const data: FetchPricesResponse = await response.json();
+      setPrices(data.Prices);
+    } catch (err) {
+      setError(err instanceof Error ? err.message : 'Unknown error');
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchPrices();
   }, []);
 
