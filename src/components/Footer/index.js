1fe8619994514c7f5edc91e7faebf43d65be852c
kirill.bushmin <kirill.bushmin@zennolab.com>
2025-07-16 15:41:44 +0300

    fixed urls

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import styles from './styles.module.css';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { localesMappings } from '../../locales/index';
 
 export default function Footer() {
   const { i18n } = useDocusaurusContext();
   const { currentLocale } = i18n;
 
-  const footerLink = `https://capmonster.cloud/footer?culture=${localesMappings[currentLocale] || localesMappings.en}`
+  const footerLink = `https://capmonster.cloud/footerFrame?culture=${currentLocale}`;
 
   return (
     <section className={styles.footerWrap}>
-          <iframe src={footerLink} />
+      <iframe src={footerLink} />
     </section>
   );
 }
