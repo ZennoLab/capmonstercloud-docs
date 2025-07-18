73b75ef45268922091b13c0675623d604e3b01b2
kirill.bushmin <kirill.bushmin@zennolab.com>
2025-07-18 12:51:36 +0300

    CCL-6438 fixed showing banner

--- src/theme/Navbar/MobileSidebar/Layout/index.js
+++ src/theme/Navbar/MobileSidebar/Layout/index.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import clsx from 'clsx';
-import {useNavbarSecondaryMenu} from '@docusaurus/theme-common/internal';
-import BadTokenBlockMobile from '@theme/BadTokenBlock/mobile';
+import { useNavbarSecondaryMenu } from '@docusaurus/theme-common/internal';
+import BadTokenBlock from '../../../BadTokenBlock';
 
-export default function NavbarMobileSidebarLayout({
-  header,
-  primaryMenu,
-  secondaryMenu,
-}) {
-  const {shown: secondaryMenuShown} = useNavbarSecondaryMenu();
+export default function NavbarMobileSidebarLayout({ header, primaryMenu, secondaryMenu }) {
+  const { shown: secondaryMenuShown } = useNavbarSecondaryMenu();
   return (
     <div className="navbar-sidebar">
       {header}
       <div
         className={clsx('navbar-sidebar__items', {
           'navbar-sidebar__items--show-secondary': secondaryMenuShown,
-        })}>
+        })}
+      >
         <div className="navbar-sidebar__item menu">{primaryMenu}</div>
         <div className="navbar-sidebar__item menu">{secondaryMenu}</div>
       </div>
-      <BadTokenBlockMobile />
+      <BadTokenBlock />
     </div>
   );
 }
