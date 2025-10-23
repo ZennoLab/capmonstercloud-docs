9c10d4cad9cb992a7a6eababad0653b9d3b8ce79
Kirill Bushmin <kirill.bushmin@zennolab.com>
2025-10-23 15:34:35 +0300

    CCL-6804 fixed link

--- src/theme/Navbar/MenuItems/SolutionSubMenu.tsx
+++ src/theme/Navbar/MenuItems/SolutionSubMenu.tsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import SubMenu from './SubMenu';
 import { HeaderMenuSingleItem } from './types';
-import RecaptchaIcon from '@site/static/img/24x24_rc.svg';
 import { uniqBy } from './utils/uniqBy';
 import { useFetchPrices } from '../../../hooks/useFetchPrices';
 import { getCaptchaLink } from './utils/getCaptchaLink';
 import { getMainLocale } from './utils/getMainLocale';
+import { useBaseUrl } from '../../../hooks/useBaseUrl.ts';
 
 const SolutionSubMenu = ({
-  isMobile,
   handleClick,
   locale,
   title,
 }: {
-  isMobile?: boolean;
   handleClick?: () => void;
   locale: string;
   title: string;
 }) => {
+  const { baseDashboardUrl, baseLandingUrl } = useBaseUrl();
+
   const { prices, loading: isPricesLoading } = useFetchPrices(false);
   const link = {
     title,
@@ -42,22 +42,15 @@ const SolutionSubMenu = ({
       {
         icon: <img src="/img/24x24_rc.svg" alt="rc" />,
         title: 'reCAPTCHA',
-        url: `https://capmonster.cloud/${getMainLocale(locale)}/recaptcha`,
+        url: `${baseLandingUrl}/${getMainLocale(locale)}/recaptcha`,
         gtmId: `header-solutions-recaptcha-btn`,
       },
       ...uniqBy(normalizePrices(prices), 'Name')?.map(priceItem => {
         return {
-          icon: (
-            <img
-              src={`https://dash.capmonster.cloud${priceItem.LogoPath}`}
-              width={24}
-              height={24}
-              alt={priceItem?.Name}
-            />
-          ),
+          icon: <img src={`${baseDashboardUrl}${priceItem.LogoPath}`} width={24} height={24} alt={priceItem?.Name} />,
           // как появится отдельная страница для Turnstile, поменять имя в базе
           title: priceItem?.Name === 'Cloudflare Turnstile' ? 'Cloudflare' : priceItem.Name,
-          url: getCaptchaLink(locale, priceItem.LinkKey, priceItem.Id),
+          url: getCaptchaLink(locale, priceItem.LinkKey, priceItem.Id, baseLandingUrl),
           gtmId: `header-solutions-${priceItem.Id}-docs-btn`,
         };
       }),
--- src/theme/Navbar/MenuItems/utils/getCaptchaLink.ts
+++ src/theme/Navbar/MenuItems/utils/getCaptchaLink.ts
@@ -6,9 +6,9 @@ export const CAPTCHA_ID_TO_PAGES: Readonly<Partial<Record<string, string>>> = {
   text: 'textcaptcha',
 } as const;
 
-export const getCaptchaLink = (currentLocale: string, LinkKey: string, Id: string) => {
+export const getCaptchaLink = (currentLocale: string, LinkKey: string, Id: string, baseLandingUrl: string) => {
   if (CAPTCHA_ID_TO_PAGES[Id]) {
-    return `https://capmonster.cloud/${getMainLocale(currentLocale)}/${CAPTCHA_ID_TO_PAGES[Id]}`;
+    return `${baseLandingUrl}/${getMainLocale(currentLocale)}/${CAPTCHA_ID_TO_PAGES[Id]}`;
   }
   return `${currentLocale === 'en' ? '' : `/${currentLocale}`}/docs/captchas/${LinkKey}`;
 };
